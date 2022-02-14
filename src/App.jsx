92a0ed17d7d4c8e661e1f68795632c33cd921aad
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-14 11:27:10 +0300

    assignment part uncommented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+// eslint-disable-next-line
 import Table from "./Table";
-/*Add import statement here*/
+import List from "./List";
 
 class App extends React.Component {
   constructor(props) {
@@ -49,7 +50,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -58,7 +59,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
